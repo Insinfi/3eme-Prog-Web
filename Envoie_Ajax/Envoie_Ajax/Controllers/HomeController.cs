8f6698652435763910cd71d9beecb3842b725938
Allan KNOCKAERT <zedsciences12@gmail.com>
2017-10-18 08:32:42 +0200

    add

---
+++ Envoie_Ajax/Envoie_Ajax/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Envoie_Ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string Identite)
+        {
+            return View();
+        }
+        public ActionResult Traitement(string Identite)
+        {
+
+            return View("Index");
+        }
+    }
+}
