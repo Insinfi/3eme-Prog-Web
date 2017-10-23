5b5f1e5a07ba30a4052bd5bb3261e276959ecc1a
Allan KNOCKAERT <zedsciences12@gmail.com>
2017-10-23 16:58:38 +0200

    01

---
+++ Arctile_UpdateDB/Arctile_UpdateDB/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Arctile_UpdateDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            AddArticle();
+            return View();
+        }
+
+        public ActionResult AddArticle()
+        {
+            return View("AddArticle");
+        }
+
+        public string Preview(HttpPostedFileBase PhotoSelect)
+        {
+            string filename = string.Empty;
+            if (PhotoSelect.ContentLength > 0)
+            {
+                filename = Path.GetFileName(PhotoSelect.FileName);
+                var path = Server.MapPath("/Images/");      //Url
+                string FullPath = path + @"\" + filename;   //Chemin
+                System.Drawing.Image sourceimage =
+                    System.Drawing.Image.FromStream(PhotoSelect.InputStream);
+                sourceimage.Save(FullPath);
+            }
+            return filename;
+        }
+    }
+}
---
+++ Arctile_UpdateDB/Arctile_UpdateDB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arctile_UpdateDB.MvcApplication" Language="C#" %>
