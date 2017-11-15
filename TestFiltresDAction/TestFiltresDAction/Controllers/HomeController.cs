5b14eb58ba8859e8a470db3d28fd681ec80cd096
Allan KNOCKAERT <zedsciences12@gmail.com>
2017-11-15 09:30:55 +0100

    Retard Retard

---
+++ TestFiltresDAction/TestFiltresDAction/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace TestFiltresDAction.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Login(string ReturnUrl)
+        {
+            if(ReturnUrl == null)
+            {
+                ReturnUrl = FormsAuthentication.DefaultUrl;
+            }
+            ViewBag.ReturnUrl = ReturnUrl;
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(Models.Identite idt)
+        {
+            if (FormsAuthentication.Authenticate(idt.Identifiant, idt.Password))
+            {
+                FormsAuthentication.SetAuthCookie(idt.Identifiant, false);
+                return new RedirectResult(idt.Redirection);
+            }
+            ViewBag.ReturnUrl = idt.Redirection;
+            return View();
+        }
+
+        public ActionResult privateEnv()
+        {
+            return View();
+        }
+    }
+}
---
+++ TestFiltresDAction/TestFiltresDAction/Models/Identite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestFiltresDAction.Models
+{
+    public class Identite
+    {
+        public string Identifiant { get; set; }
+        public string Password { get; set; }
+        public string Redirection { get; set; }
+    }
+}
